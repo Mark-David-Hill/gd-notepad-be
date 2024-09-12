41976430f01b1abc41e449687eda21461e29e739
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-09-12 17:03:08 -0600

    further sets up demo data for games

---
+++ lib/demo_data/game_demo_data.py
@@ -0,0 +1,43 @@
+from db import db
+
+from models.games import Games
+
+games_list = [
+    {
+        "name": "Super Mario Bros.",
+        "description": "A classic platformer where Mario embarks on a quest to rescue Princess Peach from Bowser.",
+        "series": "Mario",
+        "genre": "Platformer"
+    },
+    {
+        "name": "Super Mario Bros. 3",
+        "description": "An iconic platformer that introduces new power-ups and an overworld map.",
+        "series": "Mario",
+        "genre": "Platformer"
+    },
+    {
+        "name": "Super Mario World",
+        "description": "A groundbreaking platformer with expansive levels and the debut of Yoshi.",
+        "series": "Mario",
+        "genre": "Platformer"
+    },
+    {
+        "name": "Super Mario 64",
+        "description": "A revolutionary 3D platformer where Mario explores vast, open-world environments in 3D.",
+        "series": "Mario",
+        "genre": "Platformer"
+    },
+    {
+        "name": "Super Mario Odyssey",
+        "description": "A 3D platformer with innovative mechanics, including Mario's new friend Cappy, and vast, diverse worlds.",
+        "series": "Mario",
+        "genre": "Platformer"
+    }
+]
+
+def add_games():
+    for index, game in enumerate(games_list):
+        if not db.session.query(Games).filter(Games.name == name).first():
+
+            name = ""
+
