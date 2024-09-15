df36d67725479d37df235d4872e2a4679e8c9649
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-09-14 18:32:19 -0600

    adds enemy seed data for smb enemies

--- lib/demo_data/demo_data.py
+++ lib/demo_data/demo_data.py
@@ -1,9 +1,11 @@
 import sys
 
 from lib.demo_data.game_demo_data import add_games
+# from lib.demo_data.smb_demo_data.smb_enemy_demo_data import add_enemies
 
 
 def run_demo_data():
     if len(sys.argv) > 1 and sys.argv[1] == "demo-data":
         print("Creating Demo Data...")
         add_games()
+        # add_enemies()
---
+++ lib/demo_data/smb-demo-data/smb_seed_data.py
@@ -0,0 +1,72 @@
+enemy_elements = [
+    {
+        "description": "A Mario enemy",
+        "image_url": "",
+        "name": "Goomba"
+    },
+    {
+        "description": "A turtle-like enemy that can be kicked",
+        "image_url": "",
+        "name": "Koopa Troopa"
+    },
+    {
+        "description": "A faster, winged version of Koopa Troopa",
+        "image_url": "",
+        "name": "Koopa Paratroopa"
+    },
+    {
+        "description": "A spiked enemy that cannot be jumped on",
+        "image_url": "",
+        "name": "Spiny"
+    },
+    {
+        "description": "A flying, beetle-like enemy",
+        "image_url": "",
+        "name": "Buzzy Beetle"
+    },
+    {
+        "description": "A squid-like underwater enemy",
+        "image_url": "",
+        "name": "Blooper"
+    },
+    {
+        "description": "A fish that swims in underwater levels",
+        "image_url": "",
+        "name": "Cheep Cheep"
+    },
+    {
+        "description": "A fire-breathing enemy, often found in castles",
+        "image_url": "",
+        "name": "Firebar"
+    },
+    {
+        "description": "A hammer-throwing enemy",
+        "image_url": "",
+        "name": "Hammer Bro"
+    },
+    {
+        "description": "A green dragon-like enemy that shoots fireballs",
+        "image_url": "",
+        "name": "Bowser"
+    },
+    {
+        "description": "A plant that emerges from pipes",
+        "image_url": "",
+        "name": "Piranha Plant"
+    },
+    {
+        "description": "A cannonball-like projectile shot from Bill Blasters",
+        "image_url": "",
+        "name": "Bullet Bill"
+    },
+    {
+        "description": "A spiked shell enemy thrown by Lakitu",
+        "image_url": "",
+        "name": "Spiny Egg"
+    },
+    {
+        "description": "A cloud-riding enemy that throws Spinies",
+        "image_url": "",
+        "name": "Lakitu"
+    }
+]
