94ee59f3b6abf95946d3d06dba02e768b380598c
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-09-16 19:57:41 -0600

    adds demo data for types

---
+++ lib/demo_data/type_demo_data.py
@@ -0,0 +1,14 @@
+from db import db
+
+from models.types import Types
+
+types_list = ["Mechanic", "Level", "Level Element", "Enemy Element", "Power Up", "Upgrade", "Economy"]
+
+def add_types():
+    for index, name in enumerate(types_list):
+        if not db.session.query(Types).filter(Types.name == name).first():
+
+            new_type = Types(name, "example_description")
+
+            db.session.add(new_type)
+            db.session.commit()
