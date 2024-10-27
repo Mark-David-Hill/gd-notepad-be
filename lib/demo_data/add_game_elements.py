1a9ca6b6a2511885a0238c59ab68309b3cb97ebf
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-10-26 18:46:20 -0600

    fixes issue with adding relationship demo data

--- lib/demo_data/add_game_elements.py
+++ lib/demo_data/add_game_elements.py
@@ -37,8 +37,9 @@ def add_game_elements(game_name, type_name, elements_list):
         if "relationships" in game_element and game_element["relationships"]:
             for relationship in game_element["relationships"]:
                 count = relationship["count"] if "count" in relationship else 0
-                element_id_query = db.session.query(GameElements).filter(GameElements.name == relationship["name"]).first().element_id
-                new_relationship = ElementRelationships(new_game_element.element_id, element_id_query, relationship["description"], count)
+                element_1_id_query = db.session.query(GameElements).filter(GameElements.name == game_element["name"]).first().element_id
+                element_2_id_query = db.session.query(GameElements).filter(GameElements.name == relationship["name"]).first().element_id
+                new_relationship = ElementRelationships(element_1_id_query, element_2_id_query, relationship["description"], count)
                 db.session.add(new_relationship)
 
         db.session.commit()
