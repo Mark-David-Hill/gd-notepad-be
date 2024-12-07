e6d46f6644b4aaf1d91e5b37a849e44cf6f29dd8
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-12-06 20:38:47 -0700

    adds image url and 3 color fields to the types model

--- lib/demo_data/type_demo_data.py
+++ lib/demo_data/type_demo_data.py
@@ -8,7 +8,7 @@ def add_types():
     for index, name in enumerate(types_list):
         if not db.session.query(Types).filter(Types.name == name).first():
 
-            new_type = Types(name, "example_description")
+            new_type = Types(name, "example_description", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyFefeRiOb3lbMSFGyX6SGFnRD39v7GOr-mg&s", "#cccccc", "#444444", "dddddd")
 
             db.session.add(new_type)
             
--- models/types.py
+++ models/types.py
@@ -12,21 +12,27 @@ class Types(db.Model):
     type_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(), nullable=False)
     description = db.Column(db.String())
+    image_url = db.Column(db.String())
+    color = db.Column(db.String())
 
     elements = db.relationship("GameElements", foreign_keys="[GameElements.type_id]", back_populates="type", cascade='all')
     tags = db.relationship("Tags", foreign_keys="[Tags.type_id]", back_populates="type", cascade='all')
 
-    def __init__(self, name, description):
+    def __init__(self, name, description, image_url, color, light_color, dark_color):
         self.name = name
         self.description = description
+        self.image_url = image_url
+        self.color = color
+        self.light_color = light_color
+        self.dark_color = dark_color
 
     def new_type_obj():
-        return Types("", "")
+        return Types("", "", "", "", "", "", "", "")
     
 
 class TypesSchema(ma.Schema):
     class Meta:
-        fields = ['type_id', 'name', 'description']
+        fields = ['type_id', 'name', 'description', 'image_url', 'color', 'light_color', 'dark_color']
 
 
 type_schema = TypesSchema()
