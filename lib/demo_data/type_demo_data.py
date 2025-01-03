ef0c8b8fa8c9c683f2ef797426cab66e3446cb69
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2025-01-02 20:09:00 -0700

    adjusts structure of types seed data to allow for specifying values for more fields

--- lib/demo_data/type_demo_data.py
+++ lib/demo_data/type_demo_data.py
@@ -1,15 +1,25 @@
 from db import db
-
 from models.types import Types
 
-types_list = ["Mechanic", "Level", "Level Element", "Enemy Element", "Power Up"]
+types_list = [
+    {"name": "Mechanic", "image_url": "path_to_mechanic_image.jpg"},
+    {"name": "Level", "image_url": "path_to_level_image.jpg"},
+    {"name": "Level Element", "image_url": "path_to_level_element_image.jpg"},
+    {"name": "Enemy Element", "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyFefeRiOb3lbMSFGyX6SGFnRD39v7GOr-mg&s"},
+    {"name": "Power Up", "image_url": "path_to_power_up_image.jpg"}
+]
 
 def add_types():
-    for index, name in enumerate(types_list):
-        if not db.session.query(Types).filter(Types.name == name).first():
-
-            new_type = Types(name, "example_description", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyFefeRiOb3lbMSFGyX6SGFnRD39v7GOr-mg&s", "#cccccc", "#444444", "dddddd")
-
+    for type in types_list:
+        if not db.session.query(Types).filter(Types.name == type["name"]).first():
+            new_type = Types(
+                type["name"],
+                "example_description",
+                type["image_url"],
+                "#cccccc",
+                "#444444",
+                "dddddd"
+            )
             db.session.add(new_type)
-            
+    
     db.session.commit()
