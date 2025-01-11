2fd94afba5ccae3a0072a6414511cf79e773966c
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2025-01-10 18:01:27 -0700

    removes genre and series fields from the Collections model

--- lib/demo_data/collection_demo_data.py
+++ lib/demo_data/collection_demo_data.py
@@ -6,120 +6,86 @@ collections_list = [
     {
         "name": "Super Mario Bros.",
         "description": "A classic platformer where Mario embarks on a quest to rescue Princess Peach from Bowser.",
-        "series": "Mario",
-        "genre": "Platformer",
         "image_url": "https://i.ebayimg.com/images/g/eV8AAOSweOhh5i-k/s-l1200.jpg"
     },
     {
         "name": "Super Mario Bros. 3",
         "description": "An iconic platformer that introduces new power-ups and an overworld map.",
-        "series": "Mario",
-        "genre": "Platformer",
         "image_url": "https://preview.redd.it/title-screen-from-super-mario-bros-3-id-0mdjyp7t-v0-619xejuerjka1.png?width=512&format=png&auto=webp&s=b4c082e51250c2739671f7fd4333f79b8b8c214f"
     },
     {
         "name": "Super Mario World",
         "description": "A groundbreaking platformer with expansive levels and the debut of Yoshi.",
-        "series": "Mario",
-        "genre": "Platformer",
         "image_url": "https://upload.wikimedia.org/wikipedia/en/3/32/Super_Mario_World_Coverart.png"
     },
     {
         "name": "Super Mario 64",
         "description": "A revolutionary 3D platformer where Mario explores vast, open-world environments in 3D.",
-        "series": "Mario",
-        "genre": "Platformer",
         "image_url": "https://www.nintendo.com/eu/media/images/10_share_images/games_15/nintendo_7/SI_N64_SuperMario64.jpg"
     },
     {
         "name": "Super Mario Odyssey",
         "description": "A 3D platformer with innovative mechanics, including Mario's new friend Cappy, and vast, diverse worlds.",
-        "series": "Mario",
-        "genre": "Platformer",
         "image_url": "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70010000001130/c42553b4fd0312c31e70ec7468c6c9bccd739f340152925b9600631f2d29f8b5"
     },
     {
         "name": "Mega Man",
         "description": "The original Mega Man game, introducing the series' signature gameplay and six Robot Masters.",
-        "series": "Mega Man",
-        "genre": "Action/Platformer",
         "image_url": "https://upload.wikimedia.org/wikipedia/en/9/9b/Rockman_1987.jpg"
     },
     {
         "name": "Mega Man 2",
         "description": "The second game in the series, widely regarded as one of the best, featuring eight Robot Masters and refined gameplay.",
-        "series": "Mega Man",
-        "genre": "Action/Platformer",
         "image_url": "https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/Rockman_2_1988.jpg/220px-Rockman_2_1988.jpg"
     },
     {
         "name": "Mega Man 3",
         "description": "The third entry in the series, introducing the slide mechanic and Proto Man.",
-        "series": "Mega Man",
-        "genre": "Action/Platformer",
         "image_url": "https://preview.redd.it/qqep1bbbf8f81.jpg?width=640&crop=smart&auto=webp&s=339b9726731d920de81967f970f475290a0550d7"
     },
     {
         "name": "Mega Man 4",
         "description": "Mega Man's fourth adventure, which introduces the charge shot ability.",
-        "series": "Mega Man",
-        "genre": "Action/Platformer",
         "image_url": "https://e.snmc.io/lk/fv/x/8098e505aeb6ce3c2e10953ae78d2423/11877607"
     },
     {
         "name": "Mega Man 5",
         "description": "The fifth installment, with new gameplay tweaks and eight new Robot Masters.",
-        "series": "Mega Man",
-        "genre": "Action/Platformer",
         "image_url": "https://www.sprites-inc.co.uk/files/Classic/MMLC/Artwork/BoxArt/04-MM5.png"
     },
     {
         "name": "Mega Man 6",
         "description": "The sixth entry, which was the last Mega Man game for the NES, featuring Rush Adaptors for more versatile gameplay.",
-        "series": "Mega Man",
-        "genre": "Action/Platformer",
         "image_url": "https://e.snmc.io/lk/lv/x/889b818c5be6f3ce308bc85b7c9625d5/11877333"
     },
     {
         "name": "Mega Man 7",
         "description": "The first Mega Man game for the Super NES, featuring more detailed sprites and a storyline involving Bass and Treble.",
-        "series": "Mega Man",
-        "genre": "Action/Platformer",
         "image_url": "https://upload.wikimedia.org/wikipedia/en/2/27/Mega_Man_7_Coverart.jpg"
     },
     {
         "name": "Mega Man 8",
         "description": "The first and only Mega Man game on the original PlayStation, featuring 2.5D graphics and eight Robot Masters.",
-        "series": "Mega Man",
-        "genre": "Action/Platformer",
         "image_url": "https://e.snmc.io/lk/lv/x/fbfb242394c60fd6ce3d9953884bd837/5277365"
     },
     {
         "name": "Mega Man 9",
         "description": "The ninth entry returns to 8-bit graphics and mechanics, bringing back the classic NES-style gameplay.",
-        "series": "Mega Man",
-        "genre": "Action/Platformer",
         "image_url": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgtRH7KEmJxp6rfE8bla5LYrhJ7HF_Okl99QWemCbHENvh5xlMpSlY-TCeuq_Wy5VkXGXGFBIV4frIBKrAaLGgYhuUpLodnyV7CwJwyTumaUc1ANe_r5qT4uWpQ27XM8AlNbHax6fi7ORZx/s1600/mega+man+9+title+screen.jpg"
     },
     {
         "name": "Mega Man 10",
         "description": "The tenth game, also in 8-bit style, featuring a harder difficulty mode and extra playable characters.",
-        "series": "Mega Man",
-        "genre": "Action/Platformer",
         "image_url": "https://i.ytimg.com/vi/0c701F1t950/hqdefault.jpg"
     },
     {
         "name": "Mega Man 11",
         "description": "The latest entry in the series, featuring a 2.5D style, the Double Gear system, and vibrant graphics.",
-        "series": "Mega Man",
-        "genre": "Action/Platformer",
         "image_url": "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70010000003964/50637ae3b8fe8d88c7617d9fa10f8a13ddad1c3e3066d4342f286771c6403c5f"
     },
     {
         "name": "Mega Man & Bass",
         "description": "A spin-off game that allows players to choose between Mega Man and Bass, with different abilities for each character.",
-        "series": "Mega Man",
-        "genre": "Action/Platformer",
         "image_url": "https://www.nintendo.com/eu/media/images/10_share_images/games_15/virtual_console_wii_u_7/SI_WiiUVC_MegamanAndBass_image1600w.jpg"
     }
     
@@ -131,10 +97,8 @@ def add_collections():
 
             name = collection["name"]
             description = collection["description"]
-            series = collection["series"]
-            genre = collection["genre"]
             image_url = collection["image_url"]
-            new_collection = Collections(name, description, series, genre, image_url)
+            new_collection = Collections(name, description, image_url)
 
             db.session.add(new_collection)
             
--- models/collections.py
+++ models/collections.py
@@ -13,28 +13,24 @@ class Collections(db.Model):
     collection_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(), nullable=False)
     description = db.Column(db.String())
-    series = db.Column(db.String())
-    genre = db.Column(db.String())
     image_url = db.Column(db.String(), default="")
     date_created = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc), nullable=False)
 
     items = db.relationship("Items", foreign_keys="[Items.collection_id]", back_populates="collection", cascade='all')
     types = db.relationship("Types", foreign_keys="[Types.collection_id]", back_populates="collection", cascade='all')
 
-    def __init__(self, name, description, series, genre, image_url):
+    def __init__(self, name, description, image_url):
         self.name = name
         self.description = description
-        self.series = series
-        self.genre = genre
         self.image_url = image_url
 
     def new_collection_obj():
-        return Collections("", "", "", "", "")
+        return Collections("", "", "")
 
 
 class CollectionsSchema(ma.Schema):
     class Meta:
-        fields = ["collection_id", "name", "description", "series", "genre", "image_url", "date_created"]
+        fields = ["collection_id", "name", "description", "image_url", "date_created"]
 
 
 collection_schema = CollectionsSchema()
