9ff589120e439eca1c3c6ffbcbea22343839b653
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2025-01-24 20:50:40 -0700

    adjusts and adds additional colors for items in demo data

--- lib/demo_data/type_demo_data.py
+++ lib/demo_data/type_demo_data.py
@@ -5,22 +5,22 @@ from models.types import Types
 
 
 smb_types_list = [
-    {"name": "Mechanic", "image_url": "https://www.pngall.com/wp-content/uploads/5/Game-Controller-PNG-Clipart.png", "color": "#0007d1"}, 
-    {"name": "Level", "image_url": "https://www.shutterstock.com/image-vector/2d-arcade-game-level-cartoon-260nw-2259956823.jpg", "color": "#d15e00"},
-    {"name": "Level Element", "image_url": "https://i.pinimg.com/564x/6e/e9/b5/6ee9b5fdd1f67fbac5fd80445be55245.jpg", "color": "#f26d00"},
-    {"name": "Enemy Element", "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyFefeRiOb3lbMSFGyX6SGFnRD39v7GOr-mg&s", "color": "#e60400"},
-    {"name": "Power Up", "image_url": "https://static.vecteezy.com/system/resources/previews/026/973/044/non_2x/3d-icon-video-games-rendered-isolated-on-the-transparent-background-power-up-icon-for-your-design-png.png", "color": "#13e600"}
+    {"name": "Mechanic", "image_url": "https://www.pngall.com/wp-content/uploads/5/Game-Controller-PNG-Clipart.png", "color": "#2979FF"}, 
+    {"name": "Level", "image_url": "https://www.shutterstock.com/image-vector/2d-arcade-game-level-cartoon-260nw-2259956823.jpg", "color": "#FFEB3B"},
+    {"name": "Level Element", "image_url": "https://i.pinimg.com/564x/6e/e9/b5/6ee9b5fdd1f67fbac5fd80445be55245.jpg", "color": "#FFD54F"},
+    {"name": "Enemy Element", "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyFefeRiOb3lbMSFGyX6SGFnRD39v7GOr-mg&s", "color": "#E53935"},
+    {"name": "Power Up", "image_url": "https://static.vecteezy.com/system/resources/previews/026/973/044/non_2x/3d-icon-video-games-rendered-isolated-on-the-transparent-background-power-up-icon-for-your-design-png.png", "color": "#43A047"}
 ]
 
 wd_types_list = [
-    {"name": "Book", "image_url": "https://i.pinimg.com/474x/d4/d3/c0/d4d3c02f855019b7357b6c46da2124da.jpg"},
-    {"name": "Part", "image_url": "https://banner2.cleanpng.com/20180314/ikq/av0d580el.webp"},
-    {"name": "Chapter", "image_url": "https://lessonpix.com/drawings/143374/380x380/Chapter.png"},
-    {"name": "Character", "image_url": "https://t3.ftcdn.net/jpg/01/41/00/42/360_F_141004267_gvTQBCsccv1t6oSwtLG9CmzJY0mXzqeV.jpg"},
-    {"name": "Location", "image_url": "https://png.pngtree.com/png-vector/20220914/ourmid/pngtree-natural-green-hills-transparent-clipart-png-image_6174296.png"},
-    {"name": "Species", "image_url": "https://www.creativefabrica.com/wp-content/uploads/2021/12/21/1640087346/Farm-Animal-Silhouettes-black-version-580x386.jpg"},
-    {"name": "Character Group", "image_url": "https://img.freepik.com/premium-vector/set-rabbits-silhouette-drawing-white-black_1263357-4739.jpg"},
-    {"name": "Time", "image_url": "https://media.istockphoto.com/id/964947830/vector/calendar.jpg?s=612x612&w=0&k=20&c=O8oeRZpK_gTshy-acC0PkRZSrih9KXULv7ZNfLwCyHU="},
+    {"name": "Book", "color": "#4B7BE5", "image_url": "https://i.pinimg.com/474x/d4/d3/c0/d4d3c02f855019b7357b6c46da2124da.jpg"},
+    {"name": "Part", "color": "#FF9800", "image_url": "https://banner2.cleanpng.com/20180314/ikq/av0d580el.webp"},
+    {"name": "Chapter", "color": "#8E44AD", "image_url": "https://lessonpix.com/drawings/143374/380x380/Chapter.png"},
+    {"name": "Character", "color": "#E91E63", "image_url": "https://t3.ftcdn.net/jpg/01/41/00/42/360_F_141004267_gvTQBCsccv1t6oSwtLG9CmzJY0mXzqeV.jpg"},
+    {"name": "Location", "#2ECC71": "", "image_url": "https://png.pngtree.com/png-vector/20220914/ourmid/pngtree-natural-green-hills-transparent-clipart-png-image_6174296.png"},
+    {"name": "Species", "color": "#1ABC9C", "image_url": "https://www.creativefabrica.com/wp-content/uploads/2021/12/21/1640087346/Farm-Animal-Silhouettes-black-version-580x386.jpg"},
+    {"name": "Character Group", "color": "#F39C12", "image_url": "https://img.freepik.com/premium-vector/set-rabbits-silhouette-drawing-white-black_1263357-4739.jpg"},
+    {"name": "Time", "color": "#34495E", "image_url": "https://media.istockphoto.com/id/964947830/vector/calendar.jpg?s=612x612&w=0&k=20&c=O8oeRZpK_gTshy-acC0PkRZSrih9KXULv7ZNfLwCyHU="},
 ]
 
 chinese_characters_types_list = [
